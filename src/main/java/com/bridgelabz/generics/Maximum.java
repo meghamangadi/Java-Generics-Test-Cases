7fbf92604d47cfa035f2d8da276a7a2594924ef9
megha angadi <meghamangadi@gmail.com>
2021-09-08 00:19:59 +0530

    Added: Program to Refractor all the 3 types to one Generic method to find Maximum

--- src/main/java/com/bridgelabz/generics/Maximum.java
+++ src/main/java/com/bridgelabz/generics/Maximum.java
@@ -1,18 +1,14 @@
 package com.bridgelabz.generics;
 
+
 public class Maximum {
-	public static void main(String[] args) {
-		String a = "rat", b = "dog", c = "cat";
-		getmaximum(a, b, c);
-	}
-
-	public static String getmaximum(String a, String b, String c) {
-		String max = a;
-		if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
-			max = b;
-		else if (c.compareTo(max) > 0)
-			max = c;
-
-		return max;
-	}
+    public static <E extends Comparable> E getmaximum(E value1, E value2, E value3) {
+        E max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+       
+        return max;
+    }
 }
