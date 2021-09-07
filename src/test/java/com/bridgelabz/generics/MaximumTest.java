c07df192b67c035ba6c8946a00ec83b689bc5eee
megha angadi <meghamangadi@gmail.com>
2021-09-07 23:28:10 +0530

    Added: Program to find maximum float value with test case

--- src/test/java/com/bridgelabz/generics/MaximumTest.java
+++ src/test/java/com/bridgelabz/generics/MaximumTest.java
@@ -5,20 +5,20 @@ import org.junit.Test;
 
 public class MaximumTest {
 	@Test
-	public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1() {
-		int finalResult = Maximum.getmaximum(8, 3, 6);
-		Assert.assertEquals(8, finalResult);
+	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
+		float finalResult = Maximum.getmaximum(8.6f, 3.4f, 6.9f);
+		Assert.assertEquals(8.6f, finalResult, 0.0);
 	}
 
 	@Test
-	public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue2() {
-		int finalResult = Maximum.getmaximum(3, 8, 6);
-		Assert.assertEquals(8, finalResult);
+	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
+		float finalResult = Maximum.getmaximum(3.4f, 8.6f, 6.9f);
+		Assert.assertEquals(8.6f, finalResult, 0.0);
 	}
 
 	@Test
-	public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue3() {
-		int finalResult = Maximum.getmaximum(6, 3, 8);
-		Assert.assertEquals(8, finalResult);
+	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
+		float finalResult = Maximum.getmaximum(6.9f, 3.4f, 8.6f);
+		Assert.assertEquals(8.6f, finalResult, 0.0);
 	}
 }
