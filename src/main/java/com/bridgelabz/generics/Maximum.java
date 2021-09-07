434a0cdfd77b879d84a776915107f4fbbeb7b0d5
megha angadi <meghamangadi@gmail.com>
2021-09-07 23:53:38 +0530

    Added: Program to find Maximum String among given 3 string with test case

--- src/main/java/com/bridgelabz/generics/Maximum.java
+++ src/main/java/com/bridgelabz/generics/Maximum.java
@@ -1,15 +1,18 @@
 package com.bridgelabz.generics;
 
 public class Maximum {
+	public static void main(String[] args) {
+		String a = "rat", b = "dog", c = "cat";
+		getmaximum(a, b, c);
+	}
 
-	public static Float getmaximum(Float a, Float b, Float c) {
-		Float max = a;
-		if (b.compareTo(max) > 0 && b.compareTo(c) > 0) {
+	public static String getmaximum(String a, String b, String c) {
+		String max = a;
+		if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
 			max = b;
-		} else if (c.compareTo(max) > 0) {
+		else if (c.compareTo(max) > 0)
 			max = c;
-		}
-		return max;
 
+		return max;
 	}
 }
--- src/test/java/com/bridgelabz/generics/MaximumTest.java
+++ src/test/java/com/bridgelabz/generics/MaximumTest.java
@@ -1,24 +1,25 @@
 package com.bridgelabz.generics;
 
 import org.junit.Assert;
+
 import org.junit.Test;
 
 public class MaximumTest {
 	@Test
-	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-		float finalResult = Maximum.getmaximum(8.6f, 3.4f, 6.9f);
-		Assert.assertEquals(8.6f, finalResult, 0.0);
+	public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
+		String finalResult = Maximum.getmaximum("rat", "dog", "cat");
+		Assert.assertEquals("rat", finalResult);
 	}
 
 	@Test
-	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-		float finalResult = Maximum.getmaximum(3.4f, 8.6f, 6.9f);
-		Assert.assertEquals(8.6f, finalResult, 0.0);
+	public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
+		String finalResult = Maximum.getmaximum("dog", "rat", "cat");
+		Assert.assertEquals("rat", finalResult);
 	}
 
 	@Test
-	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-		float finalResult = Maximum.getmaximum(6.9f, 3.4f, 8.6f);
-		Assert.assertEquals(8.6f, finalResult, 0.0);
+	public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
+		String finalResult = Maximum.getmaximum("cat", "dog", "rat");
+		Assert.assertEquals("rat", finalResult);
 	}
 }
