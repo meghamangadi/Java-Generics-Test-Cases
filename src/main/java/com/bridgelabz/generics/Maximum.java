cdb5be2f4d980d5df77d3852771864e5d7dbff98
megha angadi <meghamangadi@gmail.com>
2021-09-08 23:00:53 +0530

    Added: Program to find maximum value among more then 3 parameters

--- src/main/java/com/bridgelabz/generics/Maximum.java
+++ src/main/java/com/bridgelabz/generics/Maximum.java
@@ -1,30 +1,27 @@
 package com.bridgelabz.generics;
 
-public class Maximum<E extends Comparable>{
-E value1;
-E value2;
-E value3;
+public class Maximum<E extends Comparable> {
 
-Maximum(E value1,E value2,E value3)
-{
-    this.value1=value1;
-    this.value2=value2;
-    this.value3=value3;
-}
+	E[] inputarray;
 
-public E getmaximum()
-{
-   E result= getmaximum(value1,value2,value3);
-    return result;
-}
-public static<E extends Comparable> E getmaximum(E value1,E value2,E value3)
-{
-    E max=value1;
-    if(value2.compareTo(max) > 0 && value2.compareTo(value3) > 0)
-        max=value2;
-    else if(value3.compareTo(max) > 0)
-        max=value3;
+	Maximum(E[] inputarray) {
+		this.inputarray = inputarray;
 
-    return max;
-}
+	}
+
+	public E getmaximum() {
+		E result = getmaximum(inputarray);
+		return result;
+	}
+
+	public static <E extends Comparable> E getmaximum(E[] inputarray) {
+		E max = inputarray[0];
+		for (E element : inputarray) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
+
+		return max;
+	}
 }
--- src/test/java/com/bridgelabz/generics/MaximumTest.java
+++ src/test/java/com/bridgelabz/generics/MaximumTest.java
@@ -4,61 +4,70 @@ import org.junit.Assert;
 
 import org.junit.Test;
 
-
-
 public class MaximumTest {
 
 	@Test
-	public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1() {
-		int finalResult = Maximum.getmaximum(8, 3, 6);
-		Assert.assertEquals(8, finalResult);
+	public void givenmorethen3Integers_WhenGetMaximum_ShouldReturnCorrectValue1() {
+		Integer[] inputarray = { 8, 3, 6, 9, 2, 5 };
+		int finalResult = Maximum.getmaximum(inputarray);
+		Assert.assertEquals(9, finalResult);
 	}
 
 	@Test
-	public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue2() {
-		int finalResult = Maximum.getmaximum(3, 8, 6);
-		Assert.assertEquals(8, finalResult);
+	public void givenmorethen3Integers_WhenGetMaximum_ShouldReturnCorrectValue2() {
+		Integer[] inputarray = { 8, 3, 6, 9, 2, 5 };
+		int finalResult = Maximum.getmaximum(inputarray);
+		Assert.assertEquals(9, finalResult);
 	}
 
 	@Test
-	public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue3() {
-		int finalResult = Maximum.getmaximum(6, 3, 8);
-		Assert.assertEquals(8, finalResult);
+	public void givenmorethen3Integers_WhenGetMaximum_ShouldReturnCorrectValue3() {
+		Integer[] inputarray = { 8, 3, 6, 9, 2, 5 };
+		int finalResult = Maximum.getmaximum(inputarray);
+		Assert.assertEquals(9, finalResult);
 	}
 
 	@Test
-	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-		float finalResult = Maximum.getmaximum(8.6f, 3.4f, 6.9f);
+	public void givenmorethen3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
+		Float[] inputarray = { 3.4f, 8.6f, 6.9f, 2.4f, 7.5f };
+		float finalResult = Maximum.getmaximum(inputarray);
+		Assert.assertEquals(8.6f, finalResult, 0.0);
+
+	}
+
+	@Test
+	public void givenmorethen3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
+		Float[] inputarray = { 8.6f, 3.4f, 6.9f, 2.4f, 7.5f };
+		float finalResult = Maximum.getmaximum(inputarray);
 		Assert.assertEquals(8.6f, finalResult, 0.0);
 	}
 
 	@Test
-	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-		float finalResult = Maximum.getmaximum(3.4f, 8.6f, 6.9f);
+	public void givenmorethen3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
+		Float[] inputarray = { 3.4f, 7.5f, 6.9f, 2.4f, 8.6f };
+		float finalResult = Maximum.getmaximum(inputarray);
 		Assert.assertEquals(8.6f, finalResult, 0.0);
 	}
 
 	@Test
-	public void given3FloatValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-		float finalResult = Maximum.getmaximum(6.9f, 3.4f, 8.6f);
-		Assert.assertEquals(8.6f, finalResult, 0.0);
+	public void givenmorethen3StringValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
+		String[] inputarray = { "rat", "def", "cdf", "dog", "cat" };
+		String finalResult = Maximum.getmaximum(inputarray);
+		Assert.assertEquals("rat", finalResult);
+
 	}
 
 	@Test
-	public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-		String finalResult = Maximum.getmaximum("rat", "dog", "cat");
+	public void givenmorethen3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
+		String[] inputarray = { "cat", "def", "cdf", "dog", "rat" };
+		String finalResult = Maximum.getmaximum(inputarray);
 		Assert.assertEquals("rat", finalResult);
 	}
 
 	@Test
-	public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-		String finalResult = Maximum.getmaximum("dog", "rat", "cat");
-		Assert.assertEquals("rat", finalResult);
-	}
-
-	@Test
-	public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-		String finalResult = Maximum.getmaximum("cat", "dog", "rat");
+	public void givenmorethen3StringValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
+		String[] inputarray = { "cdf", "def", "rat", "dog", "cat" };
+		String finalResult = Maximum.getmaximum(inputarray);
 		Assert.assertEquals("rat", finalResult);
 	}
 }
